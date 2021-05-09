42df418e5a64bc4f9f46ed346640e8531ada48d4
WARDHAN10 <erwardhan@gmail.com>
2021-05-09 12:49:17 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./styles.css";
+import quotes from "./quotedatabase";
 function App() {
+  const [values, setValues] = useState({
+    quote: "",
+    author: "",
+  });
+
+  const genrandomQuote = () => {
+    const randomNumber = Math.floor(Math.random() * quotes.length);
+    console.log(quotes[randomNumber]);
+    return quotes[randomNumber];
+  };
+  const shuffleQuotes=(array)=>{
+    return array.sort(()=>Math.random()-0.5)
+  }
+
+  const onsubmit = () => {
+    const generateRandomQuote = genrandomQuote();
+    setValues({...values,
+      quote: generateRandomQuote.quote,
+      author: generateRandomQuote.author
+    });
+    shuffleQuotes(quotes)
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div
+        className="card mx-auto text-white bg-dark col-8 "
+        style={{ marginTop: "200px" }}
+      >
+        <div className="card-body ">
+          <h3 className="card-title text-left">Quotes</h3>
+          <hr />
+          <p className="card-text text-center">
+          {values.quote}
+          </p>
+          <p className="card-text text-center">{values.author}</p>
+          <button className="btn btn-primary " onClick={onsubmit}>NEXT </button>
+        </div>
+      </div>
     </div>
   );
 }
